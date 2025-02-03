49625ed71f526448aba839fbbf237b5448db78bd
ChoHyunWoo <gusdndlek12@naver.com>
2025-02-03 12:37:29 +0900

    HttpServletRequest use

--- src/main/java/com/example/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/auth/domain/post/post/controller/ApiV1PostController.java
@@ -7,6 +7,7 @@ import com.example.auth.domain.post.post.entity.Post;
 import com.example.auth.domain.post.post.service.PostService;
 import com.example.auth.global.dto.RsData;
 import com.example.auth.global.exception.ServiceException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
-import javax.sql.rowset.serial.SerialException;
 import java.util.List;
 
 
@@ -25,6 +25,7 @@ public class ApiV1PostController {
 
     private final PostService postService;
     private final MemberService memberService;
+    private final HttpServletRequest request;
 
     @GetMapping
     public RsData<List<PostDto>> getItems() {
@@ -55,10 +56,9 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public RsData<Void> delete(@PathVariable long id,
-                               @RequestHeader("Authorization") @NotBlank String credentials) {
+    public RsData<Void> delete(@PathVariable long id) {
 
-        Member actor = getAuthenticatedActor(credentials);
+        Member actor = getAuthenticatedActor();
         Post post = postService.getItem(id).get();
 
         if (post.getAuthor().getId() != actor.getId()) {
@@ -80,11 +80,10 @@ public class ApiV1PostController {
     }
 
     @PutMapping("{id}")
-    public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body,
-                               @RequestHeader("Authorization") @NotBlank String credentials
+    public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body
     ) {
 
-        Member actor = getAuthenticatedActor(credentials);
+        Member actor = getAuthenticatedActor();
         Post post = postService.getItem(id).get();
 
         if (post.getAuthor().getId() != actor.getId()) {
@@ -107,10 +106,9 @@ public class ApiV1PostController {
     }
 
     @PostMapping
-    public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body,
-                                 @RequestHeader("Authorization") @NotBlank String credentials) {
+    public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body) {
 
-        Member actor = getAuthenticatedActor(credentials);
+        Member actor = getAuthenticatedActor();
         Post post = postService.write(actor, body.title(), body.content());
 
         return new RsData<>(
@@ -119,10 +117,12 @@ public class ApiV1PostController {
                 new PostDto(post)
         );
     }
-    private Member getAuthenticatedActor(String credentials) {
 
-        //Bearer
-         credentials = credentials.substring("Bearer ".length());
+    private Member getAuthenticatedActor() {
+        String authorizationValue = request.getHeader("Authorization");
+
+        // Bearer 4/user11234
+        String credentials = authorizationValue.substring("Bearer ".length());
 
         String[] credentialsBits = credentials.split("/");
         long authorId = Long.parseLong(credentialsBits[0]);
@@ -133,6 +133,10 @@ public class ApiV1PostController {
         if (!actor.getPassword().equals(password)) {
             throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
         }
+
         return actor;
+
     }
+
+
 }
