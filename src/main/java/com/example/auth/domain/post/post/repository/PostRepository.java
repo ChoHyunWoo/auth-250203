49eb0622cc21482f2163b90435a5f8c2cf69bc4b
ChoHyunWoo <gusdndlek12@naver.com>
2025-02-03 09:45:20 +0900

    auth Id

---
+++ src/main/java/com/example/auth/domain/post/post/repository/PostRepository.java
@@ -0,0 +1,5 @@
+package com.example.auth.domain.post.post.repository;
+import com.example.auth.domain.post.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/main/java/com/example/auth/global/app/AppConfig.java
@@ -0,0 +1,6 @@
+package com.example.auth.global.app;
+public class AppConfig {
+    public static boolean isNotProd() {
+        return true;
+    }
+}
